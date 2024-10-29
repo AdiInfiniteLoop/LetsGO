6dc14e1592d90a42e4e6a84681af48f6a264168f
AdiInfiniteLoop <adiyouaresosmart@gmail.com>
2024-10-29 08:59:26 +0530

    Lets_GO := day 2

--- booking-app/welcome.go
+++ booking-app/welcome.go
@@ -3,16 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome users to our Booking App")
-	string1 := "Variable 1"
-	fmt.Println("first part of the string", string1, "Second part of the string")
-	const constantValue = 100
-	remainingValue := 50
-	remValue := 50
-	// Non re-assignable constantValue
-	fmt.Println(constantValue)
-	fmt.Println(remainingValue)
-	fmt.Printf("The remValue id %v", remValue)
-	var input int // int means whole numbers
-	fmt.Scanf("Enter the inpu %d ", &input)
+	conferenceName := "Go Conference"
+	const conferenceTickets int32 = 50
+	var remTickets int32 = 50
+
+	fmt.Printf("Welcome to the %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and currently available are %v", conferenceTickets, remTickets)
+	fmt.Println("Get your tickets here!!")
+
+	// var userName; //Go cannot infer the dataType so we need explicit data type declaration
+	var userName string
+	var emailID string
+	var ticketBought int // use int as it auto detects the OS if it is 32 or 64
+
+	// fmt.Scan(userName) but doesn't take an input
+	// therefore we need to pass it by reference like:
+	// fmt.Scan(&userName)
+	fmt.Scanf("%s", &userName)
+	fmt.Scan(&emailID)
+	fmt.Scan(&ticketBought)
+	remTickets -= ticketBought
+	fmt.Println("Hello", userName)          // by default there is space after "Hello"
+	fmt.Println("Your emailId is", emailID) // by default there is space after "Hello"
+	fmt.Println("Successfully bought", ticketBought, "tickets")
 }
--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello GO!")
+	fmt.Println("Welcome users to our Booking App")
+	string1 := "Variable 1"
+	fmt.Println("first part of the string", string1, "Second part of the string")
+	const constantValue = 100
+	remainingValue := 50
+	remValue := 50
+	// Non re-assignable constantValue
+	fmt.Println(constantValue)
+	fmt.Println(remainingValue)
+	fmt.Printf("The remValue id %v", remValue)
+	var input int // int means whole numbers
+	fmt.Scanf("Enter the inpu %d ", &input)
+	/*var x,y,z int = 23; used for declaring multiple variables at once
+	a := 23; cannot explicitly define variables data type
+	Variables declared without a corresponding initialization are zero-valued. For example, the zero value for an int is 0.
+	*/
 }
