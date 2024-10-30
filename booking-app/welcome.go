5db139c614e2cd829f36b0ee868c522aa81f259a
AdiInfiniteLoop <adiyouaresosmart@gmail.com>
2024-10-30 19:42:00 +0530

    Array Methods

--- booking-app/welcome.go
+++ booking-app/welcome.go
@@ -6,6 +6,11 @@ func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int32 = 50
 	remTickets := 50
+	// Sizes in go have fixed length
+	bookings := [50]string{"Aditya Pradhan", "Adi"}
+	// var bookingsAlternative [50]string
+	// var emptyArray []string{}
+	// use append to make a dynamic-sized array. E.g., arr.append(43)
 
 	fmt.Printf("Welcome to the %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and currently available are %v", conferenceTickets, remTickets)
@@ -26,4 +31,14 @@ func main() {
 	fmt.Println("Hello", userName)          // by default there is space after "Hello"
 	fmt.Println("Your emailId is", emailID) // by default there is space after "Hello"
 	fmt.Println("Successfully bought", ticketBought, "tickets")
+	bookings[0] = userName
+
+	/*
+		Array printing
+	*/
+	fmt.Printf("The whole bookings array is %v", bookings)
+	fmt.Printf("The first element of the array is %v\n", bookings[0])
+	fmt.Printf("The length of the bookings array is %v\n", len(bookings))
+	fmt.Printf("The Type of the bookings array is %T\n", bookings)
+	// array types in go are defined by [size]<type>
 }
