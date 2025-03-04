b923da3ff724c017d7b9de8530194093313c9b57
AdiInfiniteLoop <adiyouaresosmart@gmail.com>
2025-03-04 21:09:19 +0530

    Loops

---
+++ basics/loops.go
@@ -0,0 +1,16 @@
+package main 
+
+import "fmt"
+
+func main() {
+  for i := 0; i < 10; i++ {
+    fmt.Println(i)
+  }
+
+  //While Loop
+  x := 0
+  for x < 10 {
+    fmt.Println(x)
+    x++
+  }
+}
