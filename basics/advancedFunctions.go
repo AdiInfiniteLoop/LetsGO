94dcaed6563ab29658c7892eabfd6ef26faed15f
AdiInfiniteLoop <adiyouaresosmart@gmail.com>
2025-03-05 13:08:56 +0530

    HOCs

--- basics/advancedFunctions.go
+++ basics/advancedFunctions.go
@@ -4,8 +4,9 @@ import "fmt"
 
 //Currying Functions: When we pass a function as an input and returns a new function
 func selfAdd(a int) func(int) int {
+  x := 80
   return func(a int) int {
-    return a + a
+    return a + a + x  //here the function is a closure also as it accesses x that is outside it's scope. so it can mutate x also.
   }
 }
 
